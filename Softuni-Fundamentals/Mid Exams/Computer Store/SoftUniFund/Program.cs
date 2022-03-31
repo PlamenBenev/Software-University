1c46d14fafb42093fad6bf37b820b040715ac1b9
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-03-31 14:24:24 +0300

    Exam task added

---
+++ Softuni-Fundamentals/Mid Exams/Computer Store/SoftUniFund/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double sum = 0;
+            bool itsDouble = false;
+
+            while (!itsDouble)
+            {
+                if (input == "special" || input == "regular")
+                {
+                    if (sum > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("Invalid order!");
+                        return;
+
+                    }
+                }
+
+                double price = double.Parse(input);
+                if (price > 0)
+                {
+                    sum += price;
+                }
+                else
+                {
+                    Console.Write("Invalid price!");
+                    Console.WriteLine();
+                }
+                input = Console.ReadLine();
+
+            }
+            double taxes = sum * 0.20;
+
+            double discountSum = sum + taxes;
+            if (input == "special")
+            {
+
+                discountSum -= discountSum * 0.1;
+            }
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {sum:f2}$");
+            Console.WriteLine($"Taxes: {taxes:f2}$");
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Total price: {discountSum:f2}$");
+        }
+    }
+}
