633f765a722cb52df2af43e10cb90ef91cb32e8d
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-02 17:58:41 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Regular Expressions (RegEx) - Exercises/Extract Emails/SoftUniFund/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @" ([A-Za-z0-9]+\.*\-*_*\w*)\@([A-Za-z]+\-*[A-Za-z]*\.[A-Za-z]+\.*[a-z]+)";
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match item in matches)
+            {
+                string it = item.Value.Trim();
+                Console.WriteLine(it);
+            }
+        }
+    }
+}
