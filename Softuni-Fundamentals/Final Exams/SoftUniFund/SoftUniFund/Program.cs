19a839ddc7b14a846b71f670a9c646002cba3f4d
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-02 17:12:51 +0300

    Exam task added

---
+++ Softuni-Fundamentals/Final Exams/SoftUniFund/SoftUniFund/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patter = @"([=]|[\/])([A-Z][A-Za-z]{2,})\1";
+            Regex regex = new Regex(patter);
+
+            string input = Console.ReadLine();
+            MatchCollection matches = regex.Matches(input);
+            int sum = 0;
+            List<string> list = new List<string>();
+
+            foreach (Match item in matches)
+            {
+                list.Add(item.Groups[2].ToString());
+                sum += item.Groups[2].ToString().Length;
+            }
+            Console.WriteLine($"Destinations: {string.Join(", ", list)}");
+            Console.WriteLine($"Travel Points: {sum}");
+        }
+    }
+}
