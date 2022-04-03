7e56f093013d90e7bb21586a98b6d2bde03485ee
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-03 21:37:29 +0300

    Exam task added

---
+++ Softuni-Fundamentals/Mid Exams/Counter-Strike/SoftUniFund/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            int won = 0;
+
+            while (input != "End of battle")
+            {
+                int num = int.Parse(input);
+
+                if (num <= energy)
+                {
+                    won++;
+                    energy -= num;
+                    if (won % 3 == 0)
+                    {
+                        energy += won;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {won} won battles and {energy} energy");
+                    return;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Won battles: {won}. Energy left: {energy}");
+        }
+
+    }
+}
