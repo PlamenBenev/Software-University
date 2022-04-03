a5d7513bd4b2c456b4113a796c63a2d864550414
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-03 21:38:58 +0300

    Exam task added

---
+++ Softuni-Fundamentals/Mid Exams/World Tour/SoftUniFund/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+
+            string input = Console.ReadLine();
+            int shotTargets = 0;
+
+            while (input != "End")
+            {
+                int index = int.Parse(input);
+
+                if (index < arr.Length)
+                {
+                    shotTargets++;
+                    int stat = arr[index];
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (arr[i] > stat && arr[i] != -1 && index != i)
+                        {
+                            arr[i] -= stat;
+                        }
+                        else if (arr[i] != -1 && index != i)
+                        {
+                            arr[i] += stat;
+                        }
+                    }
+                    arr[index] = -1;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Shot targets: {shotTargets} -> {String.Join(' ', arr)}");
+        }
+    }
+}
