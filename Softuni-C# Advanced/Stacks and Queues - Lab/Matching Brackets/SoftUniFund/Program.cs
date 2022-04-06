b9bff6f4715c2450ec01c53ac4501931f134b48d
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-06 21:56:30 +0300

    Lab task added

---
+++ Softuni-C# Advanced/Stacks and Queues - Lab/Matching Brackets/SoftUniFund/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    for (int k = stack.Peek(); k <= i; k++)
+                    {
+                        Console.Write(input[k]);
+                    }
+                    stack.Pop();
+                    Console.WriteLine();
+                }
+            }
+        }
+
+    }
+}
