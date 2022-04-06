0658925b63868597bf4755298015f32c9caa467e
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-06 19:58:49 +0300

    Lab task added

---
+++ Softuni-C# Advanced/Stacks and Queues - Lab/Softunifundamentalsmodule/SoftUniFund/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+
+    }
+}
