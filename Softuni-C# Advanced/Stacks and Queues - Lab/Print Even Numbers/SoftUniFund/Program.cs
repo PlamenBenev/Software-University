773c5ee61e75011c39100b60ce4812b20239c5c8
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-07 16:00:42 +0300

    Lab task added

---
+++ Softuni-C# Advanced/Stacks and Queues - Lab/Print Even Numbers/SoftUniFund/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    queue.Enqueue(arr[i]);
+                }
+            }
+            Console.WriteLine(String.Join(", ", queue));
+        }
+    }
+}
