cf87df597a2e99f03e0af624daa52de6a8cecda7
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-08 21:31:10 +0300

    Exam task added

---
+++ Softuni-Fundamentals/Mid Exams/Black Flag/SoftUniFund/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int dailyP = int.Parse(Console.ReadLine());
+            double expectedP = double.Parse(Console.ReadLine());
+            double gained = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+
+                gained += dailyP;
+                if (i % 3 == 0)
+                {
+                    gained += dailyP * 0.5;
+                }
+                if (i % 5 == 0)
+                {
+                    gained -= gained * 0.3;
+                }
+
+            }
+            if (gained >= expectedP)
+            {
+                Console.WriteLine($"Ahoy! {gained:f2} plunder gained.");
+            }
+            else
+            {
+                double percentage = (gained / expectedP) * 100;
+                Console.WriteLine($"Collected only {percentage:f2}% of the plunder.");
+            }
+        }
+
+    }
+}
