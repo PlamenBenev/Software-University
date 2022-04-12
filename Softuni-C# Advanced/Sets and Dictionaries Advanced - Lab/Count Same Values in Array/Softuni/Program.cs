deb69176dc30d217bd37a2fc40b9369716975adc
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-12 21:11:38 +0300

    Lab task added

---
+++ Softuni-C# Advanced/Sets and Dictionaries Advanced - Lab/Count Same Values in Array/Softuni/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> dict = new Dictionary<double, int>();
+
+            foreach (var item in arr)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                    dict.Add(item, 1);
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
