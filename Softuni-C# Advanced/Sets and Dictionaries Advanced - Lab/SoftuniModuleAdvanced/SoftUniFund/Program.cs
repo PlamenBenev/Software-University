79acadfb6d6d6cbb305d4552c428092a72f19479
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-12 21:54:41 +0300

    Lab task added

---
+++ Softuni-C# Advanced/Sets and Dictionaries Advanced - Lab/SoftuniModuleAdvanced/SoftUniFund/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] sorted = nums.OrderByDescending(x => x).ToArray();
+
+            for (int i = 0; i < sorted.Length; i++)
+            {
+                if (i < 3)
+                {
+                    Console.Write(sorted[i] + " ");
+                }
+            }
+        }
+    }
+}
