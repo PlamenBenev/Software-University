76cf31a1eecc7a780283a337d6c9af216afa231a
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-13 22:07:36 +0300

    Lab task added

---
+++ Softuni-C# Advanced/Sets and Dictionaries Advanced - Lab/Product Shop/SoftUniFund/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string,double>> dict =
+                new Dictionary<string, Dictionary<string,double>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Revision")
+            {
+                string[] token = input.Split(", ");
+
+                if (dict.ContainsKey(token[0]))
+                {
+                    dict[token[0]].Add(token[1], double.Parse(token[2]));
+                }
+                else
+                {
+                    Dictionary<string, double> secondDict = new Dictionary<string, double>();
+                    secondDict.Add(token[1], double.Parse(token[2]));
+                    dict.Add(token[0], secondDict);
+                }
+                input = Console.ReadLine();
+            }
+            var dict2 = dict.OrderBy(kvp => kvp.Key);
+            foreach (var item in dict2)
+            {
+                Console.WriteLine(item.Key + "->");
+                foreach (var product in item.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
