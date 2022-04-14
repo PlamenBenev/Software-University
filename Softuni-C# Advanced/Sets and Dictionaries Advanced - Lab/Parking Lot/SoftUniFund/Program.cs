03052aa8888e8281ef0c3b6e8a188aa275e4e982
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-14 21:01:38 +0300

    Lab task added

---
+++ Softuni-C# Advanced/Sets and Dictionaries Advanced - Lab/Parking Lot/SoftUniFund/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] token = input.Split(", ");
+
+                if (token[0] == "IN")
+                {
+                    set.Add(token[1]);
+                }
+                else if (token[0] == "OUT")
+                {
+                    set.Remove(token[1]);
+                }
+
+                input = Console.ReadLine();
+            }
+            if (set.Count > 0)
+            {
+                foreach (var item in set)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+                Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
