ec52e46cb1ddd114b1380e2b3ff224a4894d8737
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-25 10:32:53 +0300

    Exercise task added

---
+++ Softuni-C# Advanced/Stacks and Queues - Exercise/Fast Food/SoftUniFund/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Queue<int> stack = new Queue<int>();
+
+            int[] input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (var item in input)
+            {
+                stack.Enqueue(item);
+            }
+            Console.WriteLine(stack.Max());
+            while (num > 0)
+            {
+                num -= stack.First();
+                stack.Dequeue();
+                if (stack.Count == 0 || num - stack.First() < 0)
+                {
+                    break;
+                }
+            }
+            if (stack.Count > 0)
+            {
+                Console.WriteLine("Orders left: " + String.Join(" ", stack));
+            }
+            else
+                Console.WriteLine("Orders complete");
+        }
+    }
+}
