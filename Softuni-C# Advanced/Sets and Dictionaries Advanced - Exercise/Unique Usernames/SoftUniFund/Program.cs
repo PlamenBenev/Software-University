42a2d555ea5aa08d954ec9fd62635dae93fce7c6
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-27 15:31:10 +0300

    Exercise task added

---
+++ Softuni-C# Advanced/Sets and Dictionaries Advanced - Exercise/Unique Usernames/SoftUniFund/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string input = Console.ReadLine();
+
+                set.Add(input);
+            }
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
