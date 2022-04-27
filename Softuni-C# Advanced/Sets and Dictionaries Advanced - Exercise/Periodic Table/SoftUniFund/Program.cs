07904835aed7fedf2f33acc2e6063678dfaf6509
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-27 15:48:21 +0300

    Exercise task added

---
+++ Softuni-C# Advanced/Sets and Dictionaries Advanced - Exercise/Periodic Table/SoftUniFund/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            HashSet<string> chemicals = new HashSet<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] token = Console.ReadLine().Split(' ');
+
+                foreach (var item in token)
+                {
+                    chemicals.Add(item);
+                }
+            }
+            chemicals = chemicals.OrderBy(x => x).ToHashSet();
+
+            foreach (var item in chemicals)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
