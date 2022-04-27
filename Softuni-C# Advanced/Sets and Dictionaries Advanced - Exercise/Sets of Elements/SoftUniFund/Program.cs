1a40ac33199a8631cb834993da6694ff0dbde8ad
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-27 15:39:06 +0300

    Exercise task added

---
+++ Softuni-C# Advanced/Sets and Dictionaries Advanced - Exercise/Sets of Elements/SoftUniFund/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+
+            for (int i = 0; i < nums[0]; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                set1.Add(input);
+            }
+            for (int i = 0; i < nums[1]; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                set2.Add(input);
+            }
+            foreach (var item in set1)
+            {
+                if (set2.Contains(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
