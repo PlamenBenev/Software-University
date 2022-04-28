342dfb5ea902b9d1a227a2afe605f76bd961cea4
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-04-28 09:35:25 +0300

    Exercise task added

---
+++ Softuni-C# Advanced/Sets and Dictionaries Advanced - Exercise/Even Times/SoftUniFund/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Dictionary<int,int> dict = new Dictionary<int,int>();
+
+            for (int i = 0; i < num; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (dict.ContainsKey(input))
+                {
+                    dict[input]++;
+                }
+                else
+                {
+                    dict.Add(input, 1);
+                }
+            }
+            foreach (var item in dict)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
