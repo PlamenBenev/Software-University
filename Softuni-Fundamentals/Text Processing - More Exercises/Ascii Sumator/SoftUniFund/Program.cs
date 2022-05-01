0e2b48cfbfe0426e7d19ed948752cd5d43cf0fd3
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-01 22:43:43 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Text Processing - More Exercises/Ascii Sumator/SoftUniFund/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            string alabala = Console.ReadLine();
+
+            List<char> symbols = new List<char>();
+            List<int> sum = new List<int>();
+            for (int i = start + 1; i < end; i++)
+            {
+                symbols.Add((char)i);
+            }
+            foreach (var item in alabala)
+            {
+                if (symbols.Contains(item))
+                {
+                    sum.Add(item);
+                }
+            }
+            int total = sum.Sum();
+            Console.WriteLine(total);
+        }
+    }
+}
