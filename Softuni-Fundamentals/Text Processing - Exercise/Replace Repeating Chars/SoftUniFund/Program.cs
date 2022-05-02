df57e1b0889eede61b1265d6329056a620fadee7
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-02 21:05:41 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Text Processing - Exercise/Replace Repeating Chars/SoftUniFund/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = "";
+            foreach (var item in input)
+            {
+                if (output.Length > 0)
+                {
+                    if (output.Last() != item)
+                    {
+                        output += item;
+                    }
+                }
+                else
+                {
+                    output += item;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
