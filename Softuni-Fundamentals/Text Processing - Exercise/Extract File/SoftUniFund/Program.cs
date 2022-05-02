2fda13d85885bde8705e143fbfb36ecee52cbb86
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-02 21:04:09 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Text Processing - Exercise/Extract File/SoftUniFund/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] token = input.Split('\\', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] splitter = token[token.Length - 1].Split('.');
+            Console.WriteLine($"File name: {splitter[0]}");
+            Console.WriteLine($"File extension: {splitter[1]}");
+        }
+    }
+}
