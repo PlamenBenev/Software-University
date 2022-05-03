2ed92a05c22514c560b2ac729a651f99480f954a
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-03 21:06:30 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Text Processing - Exercise/Caesar Cipher/SoftUniFund/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string test = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                test += ((char)(input[i] + 3));
+            }
+            test = test.ToString();
+            Console.WriteLine(test);
+        }
+    }
+}
