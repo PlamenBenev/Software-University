056a6f395e5222e782db030f143f6bc8535ff2df
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-04 21:07:41 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/Vacation/SoftUniFund/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ppl = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string day = Console.ReadLine();
+
+            double price = 0;
+
+            if (type == "Students")
+            {
+                if (day == "Friday")
+                {
+                    price = 8.45;
+                }
+                else if (day == "Saturday")
+                {
+                    price = 9.80;
+                }
+                else if (day == "Sunday")
+                {
+                    price = 10.46;
+                }
+                if (ppl >= 30)
+                {
+                    price -= price * 0.15;
+                }
+            }
+            else if (type == "Business")
+            {
+                if (day == "Friday")
+                {
+                    price = 10.90;
+                }
+                else if (day == "Saturday")
+                {
+                    price = 15.60;
+                }
+                else if (day == "Sunday")
+                {
+                    price = 16.00;
+                }
+                if (ppl >= 100)
+                {
+                    ppl -= 10;
+                }
+            }
+            else if (type == "Regular")
+            {
+                if (day == "Friday")
+                {
+                    price = 15;
+                }
+                else if (day == "Saturday")
+                {
+                    price = 20;
+                }
+                else if (day == "Sunday")
+                {
+                    price = 22.50;
+                }
+                if (ppl >= 10 && ppl <= 20)
+                {
+                    price -= price * 0.05;
+                }
+            }
+            Console.WriteLine($"Total price: {price * ppl:f2}");
+        }
+    }
+}
