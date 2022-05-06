3e4221a47b2c692a7341e0a79aa2b307d442c277
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-06 20:35:52 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/Strong number/SoftUniFund/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, n1, s1 = 0, j;
+            int fact;
+
+            n = int.Parse(Console.ReadLine());
+            n1 = n;
+
+            for (j = n; j > 0; j = j / 10)
+            {
+
+                fact = 1;
+                for (i = 1; i <= j % 10; i++)
+                {
+                    fact = fact * i;
+                }
+                s1 = s1 + fact;
+
+            }
+            if (s1 == n1)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
