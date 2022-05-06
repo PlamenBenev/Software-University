6841c8326f99de6faf2303533932731cc0edf3ea
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-06 20:34:34 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/Login/SoftUniFund/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            int counter = 0;
+
+            char[] stringArray = username.ToCharArray();
+
+            string reverse = String.Empty;
+
+            for (int i = stringArray.Length - 1; i >= 0; i--)
+            {
+                reverse += stringArray[i];
+            }
+
+
+            while (password != reverse)
+            {
+                if (counter != 3)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                password = Console.ReadLine();
+            }
+            if (password == reverse)
+            {
+                Console.WriteLine($"User {username} logged in.");
+
+            }
+
+        }
+    }
+}
