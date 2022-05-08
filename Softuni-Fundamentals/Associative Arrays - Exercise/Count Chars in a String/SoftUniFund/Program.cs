06f78b5af48d8a285d9ad33c9d4165267b65a08f
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-08 20:22:18 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Associative Arrays - Exercise/Count Chars in a String/SoftUniFund/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, string> dic = new Dictionary<char, string>();
+
+            List<char> list = Console.ReadLine().ToList();
+
+            foreach (char c in list)
+            {
+                if (!dic.ContainsKey(c))
+                {
+                    dic[c] = string.Empty;
+                }
+                dic[c] += c;
+            }
+            foreach (var item in dic)
+            {
+                if (item.Key != ' ')
+                    Console.WriteLine(item.Key + " -> " + item.Value.Count());
+            }
+        }
+    }
+}
