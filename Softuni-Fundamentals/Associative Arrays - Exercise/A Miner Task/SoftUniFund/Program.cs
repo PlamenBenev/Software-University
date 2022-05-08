549e6a44a8bd6298804681c82e5735753e6737a6
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-08 20:23:46 +0300

    Exercise task added

---
+++ Softuni-Fundamentals/Associative Arrays - Exercise/A Miner Task/SoftUniFund/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "stop")
+            {
+                int value = int.Parse(Console.ReadLine());
+
+                if (!dic.ContainsKey(input))
+                {
+                    dic.Add(input, value);
+                }
+                else
+                    dic[input] += value;
+
+                input = Console.ReadLine();
+            }
+            foreach (var item in dic)
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value);
+            }
+        }
+    }
+}
