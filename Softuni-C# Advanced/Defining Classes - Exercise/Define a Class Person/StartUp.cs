28f4c3b9560a2ca866c0f232f26079b8dc9c4ca4
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-15 10:39:33 +0300

    Exercise task added

---
+++ Softuni-C# Advanced/Defining Classes - Exercise/Define a Class Person/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("gosho",5);
+            Person person2 = new Person("pesho", 5);
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person2.Name);
+        }
+    }
+}
