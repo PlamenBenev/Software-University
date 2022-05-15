73a3e8e02d354c3d68103ee67ebac08d74bb8ff0
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-15 13:48:32 +0300

    Exercise task added

---
+++ Softuni-C# Advanced/Defining Classes - Exercise/Opinion Poll/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(input[0], int.Parse(input[1]));
+
+                if (person.Age > 30)
+                {
+                    persons.Add(person);
+                }
+            }
+
+            persons = persons.OrderBy(x => x.Name).ToList();
+
+            foreach (var item in persons)
+            {
+                Console.WriteLine(item.Name + " - " + item.Age);
+            }
+        }
+    }
+}
