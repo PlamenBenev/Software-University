2073fa97ac4a2b02e018000e2999e096b8fd24e3
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-15 11:18:56 +0300

    Exercise task added

--- Softuni-C# Advanced/Defining Classes - Exercise/Define a Class Person/Person.cs
+++ Softuni-C# Advanced/Defining Classes - Exercise/Define a Class Person/Person.cs
@@ -10,7 +10,13 @@ namespace DefiningClasses
         private int age;
         public Person()
         {
-
+            Name = "No name";
+            Age = 1;
+        }
+        public Person(int age)
+        {
+            Name = "No name";
+            Age = age;
         }
         public Person(string name, int age)
         {
