fd87448a1fe2d9b61c5e0676965ce45a39106268
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-17 13:51:12 +0300

    Exercise task added

---
+++ Softuni-C# Advanced/Defining Classes - Exercise/Softuni Parking/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        public Car(string make, string model, int hp, string regNum)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = hp;
+            RegistrationNumber = regNum;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string RegistrationNumber { get; set; }
+        public override string ToString()
+        {
+            string result = $"Make: {this.Make}{Environment.NewLine}";
+            result += $"Model: {this.Model}{Environment.NewLine}";
+            result += $"HorsePower: {this.HorsePower}{Environment.NewLine}";
+            result += $"RegistrationNumber: {this.RegistrationNumber}";
+
+            return result;
+        }
+    }
+}
---
+++ Softuni-C# Advanced/Defining Classes - Exercise/Softuni Parking/Parking.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        public List<Car> Cars
+        {
+            get { return cars; }
+            set { this.cars = value; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+            set { this.capacity = value; }
+        }
+        public int Count
+        {
+            get { return cars.Count; }
+            
+        }
+
+        private List<Car> cars = new List<Car>();
+        private int capacity;
+        public Parking(int capp)
+        {
+            Cars = new List<Car>();
+            Capacity = capp;
+        }
+        public Parking(List<Car> cars, int capacity) : this(capacity)
+        {
+            this.Cars = new List<Car>();
+            this.Cars = cars;
+        }
+
+        public string AddCar(Car Car)
+        {
+            bool itsFine = true;
+
+            foreach (var item in cars)
+            {
+                if (item.RegistrationNumber == Car.RegistrationNumber)
+                {
+                    itsFine = false;
+                    break;
+                }
+            }
+            if (itsFine && cars.Count >= capacity)
+            {
+                return "Parking is full!";
+            }
+            if (itsFine)
+            {
+                cars.Add(Car);
+                return $"Successfully added new car {Car.Make} {Car.RegistrationNumber}";
+            }
+            else
+                return "Car with that registration number, already exists!";
+
+        }
+
+        public string RemoveCar(string RegistrationNumber)
+        {
+            bool doesItExist = false;
+            foreach (var item in cars)
+            {
+                if (item.RegistrationNumber == RegistrationNumber)
+                {
+                    cars.Remove(item);
+                    doesItExist = true;
+                    break;
+                }
+            }
+            if (!doesItExist)
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            else
+                return $"Successfully removed {RegistrationNumber}";
+        }
+        public Car GetCar(string registrationNumber)
+        {
+            Car ccar = new Car("", "", 0, "");
+            foreach (var item in cars)
+            {
+                if (item.RegistrationNumber == registrationNumber)
+                {
+                    ccar = item;
+                    break;
+                }
+            }
+            return ccar;
+        }
+
+        public int RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+            for (int i = 0; i < Cars.Count; i++)
+            {
+                if (RegistrationNumbers.Contains(this.Cars[i].RegistrationNumber))
+                {
+                    this.Cars.RemoveAt(i);
+                    i--;
+                }
+            }
+            return cars.Count;
+        }
+    }
+}
