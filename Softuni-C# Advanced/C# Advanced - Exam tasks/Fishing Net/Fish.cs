126af7ec37cdc32ada1fdd89e782b0797e273029
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-21 16:24:52 +0300

    Exam task added

---
+++ Softuni-C# Advanced/C# Advanced - Exam tasks/Fishing Net/Fish.cs
@@ -0,0 +1,19 @@
+﻿namespace FishingNet
+{
+    public class Fish
+    {
+        public string FishType { get; set; }
+        public double Length { get; set; }
+        public double Weight { get; set; }
+        public Fish(string type,double length,double weight)
+        {
+            FishType = type;
+            Length = length;
+            Weight = weight;
+        }
+        public override string ToString()
+        {
+            return $"There is a {FishType}, {Length} cm. long, and {Weight} gr. in weight.";
+        }
+    }
+}
---
+++ Softuni-C# Advanced/C# Advanced - Exam tasks/Fishing Net/Net.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        public List<Fish> Fish { get; set; }
+        public string Material { get; set; }
+        public int Capacity { get; set; }
+        public Net(string material, int capacity)
+        {
+            Fish = new List<Fish>();
+            Material = material;
+            Capacity = capacity;
+        }
+        public int Count
+        {
+            get { return Fish.Count; }
+        }
+        public string AddFish(Fish fish)
+        {
+            if (string.IsNullOrWhiteSpace(fish.FishType) || fish.Length <= 0 &&
+                fish.Weight <= 0)
+            {
+                return "Invalid fish.";
+            }
+            if (Fish.Count >= Capacity)
+            {
+                return "Fishing net is full.";
+            }
+            Fish.Add(fish);
+            return $"Successfully added {fish.FishType} to the fishing net.";
+        }
+        public bool ReleaseFish(double weight)
+        {
+            foreach (var item in Fish)
+            {
+                if (item.Weight == weight)
+                {
+                    Fish.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Fish GetFish(string fishType)
+        {
+            Fish fish = null;
+            foreach (var item in Fish)
+            {
+                if (item.FishType == fishType)
+                {
+                   fish = item;
+                }
+            }
+            return fish;
+        }
+        public Fish GetBiggestFish()
+        {
+            Fish fish = null;
+            double longest = 0;
+            foreach (var item in Fish)
+            {
+                if (item.Length > longest)
+                {
+                    longest = item.Length;
+                    fish = item;
+                }
+            }
+            return fish;
+        }
+        public string Report()
+        {
+            List<Fish> orderList = Fish.OrderByDescending(x => x.Length).ToList();
+
+            return $"Into the cast net:{Environment.NewLine}{String.Join(Environment.NewLine, orderList)}";
+
+
+        }
+    }
+}
