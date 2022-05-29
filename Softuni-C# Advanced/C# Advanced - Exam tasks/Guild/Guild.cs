e4e679af6101cdcf7878d862f4512a58ed9d6efa
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-05-29 15:27:16 +0300

    Exam task added

---
+++ Softuni-C# Advanced/C# Advanced - Exam tasks/Guild/Guild.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        public List<Player> Roaster { get; set; } = new List<Player>();
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            if (Capacity > Roaster.Count)
+            {
+                Roaster.Add(player);
+            }
+        }
+        public bool RemovePlayer(string name)
+        {
+            foreach (var item in Roaster)
+            {
+                if (item.Name == name)
+                {
+                    Roaster.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void PromotePlayer(string name)
+        {
+            foreach (var item in Roaster)
+            {
+                if (item.Name == name)
+                {
+                    if (item.Rank != "Member")
+                    {
+                        item.Rank = "Member";
+                    }
+                    break;
+                }
+            }
+        }
+        public void DemotePlayer(string name)
+        {
+            foreach (var item in Roaster)
+            {
+                if (item.Name == name)
+                {
+                    if (item.Rank != "Trial")
+                    {
+                        item.Rank = "Trial";
+                    }
+                    break;
+                }
+            }
+        }
+        public Player[] KickPlayersByClass(string classs)
+        {
+            List<Player> list = new List<Player>();
+
+            for (int i = 0; i < Roaster.Count; i++)
+            {
+                if (Roaster[i].Class == classs)
+                {
+                    list.Add(Roaster[i]);
+                    Roaster.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Player[] kicked = list.ToArray();
+            return kicked;
+        }
+        public int Count
+        {
+            get { return Roaster.Count; }
+        }
+        public string Report()
+        {
+            string returner = $"Players in the guild: {Name}{Environment.NewLine}" +
+                $"{string.Join(Environment.NewLine, Roaster)}";
+
+            return returner;
+        }
+    }
+}
---
+++ Softuni-C# Advanced/C# Advanced - Exam tasks/Guild/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+
+        public Player(string name, string classs)
+        {
+            Name = name;
+            Class = classs;
+            Rank = "Trial";
+            Description = "n/a";
+        }
+
+        public override string ToString()
+        {
+            string returner = $"Player {Name}: {Class}{Environment.NewLine}" +
+                $"Rank: {Rank}{Environment.NewLine}" +
+                $"Description: {Description}";
+
+            return returner;
+        }
+    }
+}
