f44353dbad6eb3e8bf1127ff46ad9f427edd693c
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-06-12 16:10:18 +0300

    Exercise task added

---
+++ Softuni-C# OOP/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            DarkKnight darkKnight = new DarkKnight("Gosho",14);
+            Console.WriteLine(darkKnight);
+        }
+    }
+}
