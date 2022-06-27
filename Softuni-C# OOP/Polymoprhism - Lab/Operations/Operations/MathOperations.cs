30b8f3a9d8de35dacab0fac5d57782a6b2fc355c
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-06-27 14:42:54 +0300

    Lab task added

---
+++ Softuni-C# OOP/Polymoprhism - Lab/Operations/Operations/MathOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int s, int d)
+        {
+            return s + d;
+        }
+        public double Add(double s , double d , double f)
+        {
+            return s + d + f;
+        }
+        public decimal Add(decimal s , decimal d , decimal f)
+        {
+            return s + d + f;
+        }
+    }
+}
