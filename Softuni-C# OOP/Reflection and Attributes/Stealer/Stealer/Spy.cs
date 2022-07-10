db71dcb6edfe606c68ff1c8163064593f9fd656d
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-07-10 14:51:13 +0300

    Lab task added

---
+++ Softuni-C# OOP/Reflection and Attributes/Stealer/Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy : Hacker
+    {
+        public string StealFieldInfo(string className, params string[] fields)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static
+                | BindingFlags.NonPublic | BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType , new object[] {});
+
+            sb.AppendLine($"Class under investigation: {className}");
+
+            foreach (var item in classFields.Where(x => fields.Contains(x.Name)))
+            {
+                sb.AppendLine($"{item.Name} = {item.GetValue(classInstance)}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Softuni-C# OOP/Reflection and Attributes/Stealer/Stealer/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stealer
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            Console.WriteLine(spy.StealFieldInfo("Stealer.Hacker", "username","password"));
+        }
+    }
+}
