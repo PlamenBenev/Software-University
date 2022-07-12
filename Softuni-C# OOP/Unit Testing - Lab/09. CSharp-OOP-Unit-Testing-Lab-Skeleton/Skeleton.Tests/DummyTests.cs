f518f2b858d5348c523bfff49853e18d18ed2215
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-07-12 16:02:41 +0300

    Lab task added

--- Softuni-C# OOP/Unit Testing - Lab/09. CSharp-OOP-Unit-Testing-Lab-Skeleton/Skeleton.Tests/DummyTests.cs
+++ Softuni-C# OOP/Unit Testing - Lab/09. CSharp-OOP-Unit-Testing-Lab-Skeleton/Skeleton.Tests/DummyTests.cs
@@ -6,9 +6,42 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void LosingHealth()
         {
+            Dummy dummy = new Dummy(11, 10);
 
+            dummy.TakeAttack(10);
+
+            Assert.True(dummy.Health == 1);
+        }
+        [Test]
+        public void DeadDummyExeptionWhenAttacked()
+        {
+            Dummy dummy = new Dummy(1, 10);
+
+            dummy.TakeAttack(10);
+
+            Assert.That(() => dummy.TakeAttack(10), Throws.InvalidOperationException
+                .With.Message.EqualTo("Dummy is dead."));
+        }
+        [Test]
+        public void AliveDummyCantGiveXP()
+        {
+            Dummy dummy = new Dummy(11, 10);
+
+            dummy.TakeAttack(10);
+
+            Assert.That(() => dummy.GiveExperience(), Throws.InvalidOperationException
+                .With.Message.EqualTo("Target is not dead."));
+        }
+        [Test]
+        public void DeadDummyCanGiveXP()
+        {
+            Dummy dummy = new Dummy(1, 10);
+
+            dummy.TakeAttack(10);
+
+            Assert.True(dummy.GiveExperience() == 10);
         }
     }
 }
