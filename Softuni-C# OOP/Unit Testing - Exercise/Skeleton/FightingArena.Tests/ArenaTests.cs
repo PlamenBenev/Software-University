a564d0e9fbc9e6a5db084ccc86ca2ce281a08089
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-07-16 15:50:44 +0300

    Exercise task added

--- Softuni-C# OOP/Unit Testing - Exercise/Skeleton/FightingArena.Tests/ArenaTests.cs
+++ Softuni-C# OOP/Unit Testing - Exercise/Skeleton/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,77 @@
 ﻿namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System.Collections.Generic;
 
     [TestFixture]
     public class ArenaTests
     {
+        Arena arena = new Arena();
+        [Test]
+        public void IsArenaNull()
+        {
+            Assert.True(arena != null);
+        }
+        [Test]
+        public void MakingCollection()
+        {
+            List<Warrior> list = new List<Warrior>();
 
+            CollectionAssert.AreEqual(arena.Warriors,  list);
+        }
+        [Test]
+        public void CountAndAddingInTheCollection()
+        {
+            Warrior war1 = new Warrior("goshoGladiatora", 5, 4);
+            Warrior war2 = new Warrior("peshoGladiatora", 3, 6);
+
+            Arena newArena = new Arena();
+            newArena.Enroll(war1);
+            newArena.Enroll(war2);
+
+            Assert.True(newArena.Count == 2);
+        }
+        [Test]
+        public void ExeptionIfTheNameExist()
+        {
+            Warrior war1 = new Warrior("goshoGladiatora", 5, 4);
+            Warrior war2 = new Warrior("goshoGladiatora", 3, 6);
+
+            Arena newArena = new Arena();
+            newArena.Enroll(war1);
+
+            Assert.That(() => newArena.Enroll(war2)
+            , Throws.InvalidOperationException);
+        }
+        [Test]
+        public void FightExeption()
+        {
+            Warrior war1 = new Warrior("goshoGladiatora", 5, 4);
+            Warrior war2 = new Warrior("peshoGladiatora", 3, 6);
+
+            Arena newArena = new Arena();
+            newArena.Enroll(war1);
+            newArena.Enroll(war2);
+
+            Assert.That(() => newArena.Fight(null,"peshoGladiatora")
+            ,Throws.InvalidOperationException);
+            Assert.That(() => newArena.Fight("peshoGladiatora", null)
+            , Throws.InvalidOperationException);
+            Assert.That(() => newArena.Fight(null, null)
+            , Throws.InvalidOperationException);
+        }
+        [Test]
+        public void DoesFightTakeDamage()
+        {
+            Warrior war1 = new Warrior("goshoGladiatora", 30, 31);
+            Warrior war2 = new Warrior("peshoGladiatora", 30, 32);
+
+            Arena newArena = new Arena();
+            newArena.Enroll(war1);
+            newArena.Enroll(war2);
+            newArena.Fight("peshoGladiatora", "goshoGladiatora");
+
+            Assert.True(war1.HP == 1);
+        }
     }
 }
--- Softuni-C# OOP/Unit Testing - Exercise/Skeleton/FightingArena.Tests/WarriorTests.cs
+++ Softuni-C# OOP/Unit Testing - Exercise/Skeleton/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,89 @@
 namespace FightingArena.Tests
 {
     using NUnit.Framework;
-
+    using System;
     [TestFixture]
     public class WarriorTests
     {
 
+        [Test]
+        public void IsNameNull()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior war = new Warrior(" ", 30, 31);
+                Warrior war2 = new Warrior(null, 30, 31);
+            });
+        }
+        [Test]
+        public void DamageIsPositive()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior war = new Warrior("pesho", -30, 31);
+            });
+        }
+        [Test]
+        public void HPIsPositive()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior war = new Warrior("pesho", 30, -31);
+            });
+        }
+        [Test]
+        public void IfAttackerHPIsTooLowExeption()
+        {
+            Warrior war = new Warrior("pesho", 30, 29);
+            Warrior def = new Warrior("pesho", 30, 41);
+
+            Assert.That(() => war.Attack(def),
+                Throws.InvalidOperationException);
+        }
+        [Test]
+        public void IfDefenderHPIsTooLowExeption()
+        {
+            Warrior war = new Warrior("pesho", 30, 30);
+            Warrior def = new Warrior("pesho", 30, 29);
+
+            Assert.That(() => war.Attack(def),
+                Throws.InvalidOperationException);
+        }
+        [Test]
+        public void IfTheEnemyIsTooStrongExeption()
+        {
+            Warrior war = new Warrior("pesho", 30, 30);
+            Warrior def = new Warrior("pesho", 31, 35);
+
+            Assert.That(() => war.Attack(def),
+                Throws.InvalidOperationException);
+        }
+        [Test]
+        public void IfAttackerTakeDamage()
+        {
+            Warrior war = new Warrior("pesho", 32, 36);
+            Warrior def = new Warrior("pesho", 31, 35);
+            war.Attack(def);
+
+            Assert.True(war.HP == 5);
+        }
+        [Test]
+        public void IfDefenderDies()
+        {
+            Warrior war = new Warrior("pesho", 32, 36);
+            Warrior def = new Warrior("pesho", 31, 31);
+            war.Attack(def);
+
+            Assert.True(def.HP == 0);
+        }
+        [Test]
+        public void IfDefenderTakeDamage()
+        {
+            Warrior war = new Warrior("pesho", 32, 36);
+            Warrior def = new Warrior("pesho", 31, 33);
+            war.Attack(def);
+
+            Assert.True(def.HP == 1);
+        }
     }
 }
