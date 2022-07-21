1ff367d58499d661e338eb422404feb00f235895
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-07-21 16:27:35 +0300

    Exam task added

---
+++ Softuni-C# OOP/Exam Tasks/AquaShop/03. Unit Tests_Skeleton (1)/Aquariums.Tests/AquariumsTests.cs
@@ -0,0 +1,128 @@
+﻿namespace Aquariums.Tests
+{
+    using NUnit.Framework;
+    using System;
+    [TestFixture]
+    public class AquariumsTests
+    {
+        private Aquarium aquarium = null;
+        [Test]
+        public void NameExeption()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                aquarium = new Aquarium(null, 5);
+            });
+        }
+        [Test]
+        public void CapacityExeption()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                aquarium = new Aquarium("aqua", -1);
+            });
+        }
+        [Test]
+        public void ProperlyParamsReturn()
+        {
+            aquarium = new Aquarium("aqua", 5);
+
+            Assert.True(aquarium.Name == "aqua");
+            Assert.True(aquarium.Capacity == 5);
+        }
+        [Test]
+        public void AquariumFishCount()
+        {
+            aquarium = new Aquarium("aqua", 5);
+            Fish fish = new Fish("nemo");
+
+            aquarium.Add(fish);
+
+            Assert.True(aquarium.Count == 1);
+        }
+        [Test]
+        public void AddingFishException()
+        {
+            aquarium = new Aquarium("aqua", 1);
+            Fish fish = new Fish("nemo");
+            Fish fish2 = new Fish("Canko");
+
+            aquarium.Add(fish);
+
+            Assert.That(() => aquarium.Add(fish2),
+                Throws.InvalidOperationException);
+        }
+        [Test]
+        public void RemoveFishException()
+        {
+            aquarium = new Aquarium("aqua", 1);
+            Fish fish = new Fish("nemo");
+
+            aquarium.Add(fish);
+
+            Assert.That(() => aquarium.RemoveFish("nemoo"),
+                Throws.InvalidOperationException);
+        }
+        [Test]
+        public void RemoveFishProperlyReturn()
+        {
+            aquarium = new Aquarium("aqua", 1);
+            Fish fish = new Fish("nemo");
+
+            aquarium.Add(fish);
+            aquarium.RemoveFish("nemo");
+            Assert.True(aquarium.Count == 0);
+        }
+        [Test]
+        public void SellFishException()
+        {
+            aquarium = new Aquarium("aqua", 1);
+            Fish fish = new Fish("nemo");
+
+            aquarium.Add(fish);
+
+            Assert.That(() => aquarium.SellFish("nemoo"),
+                Throws.InvalidOperationException);
+        }
+        [Test]
+        public void AvalibleAfterSellFish()
+        {
+            aquarium = new Aquarium("aqua", 1);
+            Fish fish = new Fish("nemo");
+
+            aquarium.Add(fish);
+            aquarium.SellFish("nemo");
+
+            Assert.True(fish.Available == false);
+        }
+        [Test]
+        public void SellFishReturnsTheRequestedFish()
+        {
+            aquarium = new Aquarium("aqua", 1);
+            Fish fish = new Fish("nemo");
+
+            aquarium.Add(fish);
+
+            Assert.True(aquarium.SellFish("nemo") == fish);
+        }
+        [Test]
+        public void ReportReturnProperly()
+        {
+            aquarium = new Aquarium("aqua", 2);
+            Fish fish = new Fish("nemo");
+            Fish fish2 = new Fish("Canko");
+
+            aquarium.Add(fish);
+            aquarium.Add(fish2);
+
+            Assert.True(aquarium.Report() == $"Fish available at aqua: nemo, Canko");
+        }
+        [Test]
+        public void FishParamsAreCorrect()
+        {
+            Fish nemo = new Fish("Nemo");
+            Assert.True(nemo.Name == "Nemo");
+            Assert.True(nemo.Available == true);
+        }
+    }
+}
---
+++ Softuni-C# OOP/Exam Tasks/AquaShop/Core/Contracts/Controller.cs
@@ -0,0 +1,164 @@
+﻿using AquaShop.Models.Aquariums;
+using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations;
+using AquaShop.Models.Fish;
+using AquaShop.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Core.Contracts
+{
+    public class Controller : IController
+    {
+        private DecorationRepository decRep = new DecorationRepository();
+        private List<IAquarium> aquarium = new List<IAquarium>();
+        public string AddAquarium(string aquariumType, string aquariumName)
+        {
+            IAquarium aquar = null;
+            if (aquariumType != "SaltwaterAquarium" &&
+                aquariumType != "FreshwaterAquarium")
+            {
+                throw new InvalidOperationException("Invalid aquarium type.");
+            }
+            if (aquariumType == "SaltwaterAquarium")
+            {
+                aquar = new SaltwaterAquarium(aquariumName);
+            }
+            else if (aquariumType == "FreshwaterAquarium")
+            {
+                aquar = new FreshwaterAquarium(aquariumName);
+            }
+            aquarium.Add(aquar);
+            return $"Successfully added {aquariumType}.";
+        }
+
+        public string AddDecoration(string decorationType)
+        {
+            if (decorationType != "Ornament" &&
+                decorationType != "Plant")
+            {
+                throw new InvalidOperationException("Invalid decoration type.");
+            }
+            if (decorationType == "Ornament")
+            {
+                decRep.Add(new Ornament());
+            }
+            else
+            {
+                decRep.Add(new Plant());
+            }
+            return $"Successfully added {decorationType}.";
+        }
+
+        public string AddFish(string aquariumName, string fishType, string fishName, string fishSpecies, decimal price)
+        {
+            if (fishType != "FreshwaterFish" &&
+                fishType != "SaltwaterFish")
+            {
+                throw new InvalidOperationException("Invalid fish type.");
+            }
+
+            foreach (var item in aquarium)
+            {
+                if (item.Name == aquariumName)
+                {
+                    string type = item.GetType().Name;
+                    if (fishType == "FreshwaterFish" && type == "FreshwaterAquarium")
+                    {
+                        item.AddFish(new FreshwaterFish(fishName, fishSpecies, price));
+                    }
+                    else if (fishType == "SaltwaterFish" && type == "SaltwaterAquarium")
+                    {
+                        item.AddFish(new SaltwaterFish(fishName, fishSpecies, price));
+                    }
+                    else
+                    {
+                        return "Water not suitable.";
+                    }
+                }
+            }
+            return $"Successfully added {fishType} to {aquariumName}.";
+        }
+
+        public string CalculateValue(string aquariumName)
+        {
+            decimal price = 0;
+            foreach (var item in aquarium)
+            {
+                if (item.Name == aquariumName)
+                {
+                    foreach (var fish in item.Fish)
+                    {
+                        price += fish.Price;
+                    }
+                    foreach (var decor in item.Decorations)
+                    {
+                        price += decor.Price;
+                    }
+                }
+            }
+            return $"The value of Aquarium {aquariumName} is {price:f2}.";
+        }
+
+        public string FeedFish(string aquariumName)
+        {
+            int fedCount = 0;
+            foreach (var item in aquarium)
+            {
+                if (item.Name == aquariumName)
+                {
+                    item.Feed();
+                    fedCount = item.Fish.Count;
+                }
+            }
+            return $"Fish fed: {fedCount}";
+        }
+
+        public string InsertDecoration(string aquariumName, string decorationType)
+        {
+            string type = null;
+
+            foreach (var item in decRep.Models)
+            {
+                if (item.GetType().Name == decorationType)
+                {
+                    type = item.GetType().Name;
+                }
+            }
+            if (type == null)
+            {
+                throw new InvalidOperationException($"There isn't a decoration of type {decorationType}.");
+            }
+            Decoration decorationToAdd = null;
+            foreach (var item in decRep.Models)
+            {
+                if (item.GetType().Name == decorationType)
+                {
+                    decorationToAdd = (Decoration)item;
+                    decRep.Remove(item);
+                    break;
+                }
+            }
+            foreach (var name in aquarium)
+            {
+                if (name.Name == aquariumName)
+                {
+                    name.AddDecoration(decorationToAdd);
+                }
+            }
+            return $"Successfully added {decorationType} to {aquariumName}.";
+        }
+
+        public string Report()
+        {
+            string info = null;
+
+            foreach (var item in aquarium)
+            {
+                info += item.GetInfo() + Environment.NewLine;
+            }
+            return info;
+        }
+    }
+}
