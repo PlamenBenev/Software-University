61d573fc6f52ef45eebb8cb859f284b44b9b2e74
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-09-03 15:12:01 +0300

    Lab task added

---
+++ SoftUni-JS-Advanced/Lab-Syntax, Functions and Statements/javascript - taks 2.js
@@ -0,0 +1,6 @@
+function asd(first,second,thirth){
+    console.log(first.length + second.length + thirth.length);
+    let avrg = (first.length + second.length + thirth.length) / 3
+    console.log(Math.floor(avrg));
+}
+asd('chocolate', 'ice cream', 'cake');
