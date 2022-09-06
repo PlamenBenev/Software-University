783403c91c7ec2d4f51686e3b8e136fc7b2ed782
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-09-06 15:07:44 +0300

    lab task added

--- SoftUni-JS-Advanced/Lab - Arrays and Nested Arrays/javascript - task 6.js
+++ SoftUni-JS-Advanced/Lab - Arrays and Nested Arrays/javascript - task 6.js
@@ -1,16 +1,22 @@
  function asd(input){
     let array = [];
-    let result = [];
     for (let index = 0; index < input.length; index++) {
         array[index] = input[index];
     }
+    let result = [];
+
     array.sort((a, b) => a - b);
 
     if (array.length % 2 != 0){
-        for (let index = (array.length - 1) / 2; index < array.length; index++) {
+        for (let index = (input.length - 1) / 2; index < input.length; index++) {
+            result.push(array[index]);
+        }
+    } else {
+        for (let index = (input.length ) / 2; index < input.length; index++) {
             result.push(array[index]);
         }
     }
+    
     console.log(result);
 }
 asd([3, 19, 14, 7, 2, 19, 6]);
