ae2320030366b7146cf552b3a9145ba7e7c246db
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-09-07 13:11:18 +0300

    lab task added

---
+++ SoftUni-JS-Advanced/Lab - Objects and Composition/javascript - task 1.js
@@ -0,0 +1,9 @@
+ function asd(name,population,treasary){
+    const city = {};
+    city.name = name;
+    city.population = population;
+    city.treasary = treasary;
+
+    return city;
+}
+console.log(asd('Tortuga',7000,15000));
