918b7d056594287bfd95c7f9332c3341bc595d88
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-09-14 11:07:12 +0300

    Lab task added

---
+++ SoftUni-JS-Advanced/Lab - DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let getText = document.getElementById('text');
+    let removeMore = document.getElementById('more');
+
+    getText.style.display = 'inline';
+    removeMore.style.display = 'none';
+}
