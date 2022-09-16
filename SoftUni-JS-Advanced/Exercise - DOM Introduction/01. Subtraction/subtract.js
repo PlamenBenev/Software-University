95662a6a39bdcd8ead3135918f85b6b0e0437265
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-09-16 13:58:49 +0300

    Exercise task added

---
+++ SoftUni-JS-Advanced/Exercise - DOM Introduction/01. Subtraction/subtract.js
@@ -0,0 +1,7 @@
+function subtract() {
+    let result = document.getElementById('result');
+    let num1 = document.getElementById('firstNumber').value;
+    let num2 = document.getElementById('secondNumber').value;
+
+    result.textContent = Number(num1) - Number(num2);
+}
