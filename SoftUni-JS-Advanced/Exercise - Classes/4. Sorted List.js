320c29d98c205fe91b372264498fdd4125602703
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-10-05 19:30:03 +0300

    Exercise task added

--- SoftUni-JS-Advanced/Exercise - Classes/4. Sorted List.js
+++ SoftUni-JS-Advanced/Exercise - Classes/4. Sorted List.js
@@ -1,24 +1,30 @@
 class List {
   constructor() {
-    this.list = [].sort();
-    this.size = this.list.length;
+    this.list = [];
+    this.size = 0;
   }
 
   add(element){
     this.list.push(element);
+    this.size++;
+    this.list.sort((a, b) => a - b);
   }
   remove(index){
-    this.list.splice(index,index);
+    if (index >= 0 && index < this.list.length) {
+      this.list.splice(index,1);
+      this.size--;
+      this.list.sort((a, b) => a - b);
+    }
   }
   get(index){
-    return this.list[index];
+    if (index >= 0 && index < this.list.length)  {
+      return this.list[index];
+    }
   }
 }
 
 let list = new List();
 list.add(5);
-list.add(6);
-list.add(7);
-console.log(list.get(1));
-list.remove(1);
-console.log(list.get(1));
+list.add(3);
+list.remove(0);
+console.log(list.get(0));
