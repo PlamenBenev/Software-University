140056f1a6fb93a1d5b372639efd6a124155746e
PlamenBenev <102227630+PlamenBenev@users.noreply.github.com>
2022-10-28 10:58:12 +0300

    Exercise task added

--- JS Applications/Exercise Data and Authentication/04.Book-library/app.js
+++ JS Applications/Exercise Data and Authentication/04.Book-library/app.js
@@ -1,19 +1,19 @@
-function solve(){
+function solve() {
     let url = `http://localhost:3030/jsonstore/collections/books`;
 
-    document.querySelector('form button').addEventListener('click',(e)=>{
+    document.querySelector('form button').addEventListener('click', (e) => {
+        e.preventDefault();
         let title = document.getElementById('title');
         let author = document.getElementById('author');
-    
-        if (title.value == '' || author.value == ''){
+
+        if (title.value == '' || author.value == '') {
             return;
         }
-        e.preventDefault();
-        createBook(url,title,author);
+        createBook(url, title, author);
         loadBooks(url);
 
     })
-    document.getElementById('loadBooks').addEventListener('click',(e)=>{
+    document.getElementById('loadBooks').addEventListener('click', (e) => {
         e.preventDefault();
 
         loadBooks(url);
@@ -21,7 +21,7 @@ function solve(){
 }
 solve();
 
-function createBook(url,title,author){
+function createBook(url, title, author) {
 
     let request = {
         author: author.value,
@@ -30,65 +30,89 @@ function createBook(url,title,author){
 
     fetch(url, {
         method: 'post',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(request)
     })
     title.value = '';
     author.value = '';
 }
 
-function loadBooks(url){
+function loadBooks(url) {
     fetch(url).then(x => x.json())
-    .then(x => {
-        document.querySelector('tbody').innerHTML = '';
-        Object.entries(x).forEach(r => {
-            let tr = document.createElement('tr');
-            let td1 = document.createElement('td');
-            let td2 = document.createElement('td');
-            let td3 = document.createElement('td');
-            let editBtn = document.createElement('button');
-            let deleteBtn = document.createElement('button');
+        .then(x => {
+            document.querySelector('tbody').innerHTML = '';
+            Object.entries(x).forEach(r => {
+                let tr = document.createElement('tr');
+                let td1 = document.createElement('td');
+                let td2 = document.createElement('td');
+                let td3 = document.createElement('td');
+                let editBtn = document.createElement('button');
+                let deleteBtn = document.createElement('button');
 
-            td1.textContent = r[1].author;
-            td2.textContent = r[1].title;
-            editBtn.textContent = 'Edit';
-            deleteBtn.textContent = 'Delete';
-            td3.appendChild(editBtn);
-            td3.appendChild(deleteBtn);
-        
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
-        
-            document.querySelector('tbody').appendChild(tr);
+                td1.textContent = r[1].author;
+                td2.textContent = r[1].title;
+                editBtn.textContent = 'Edit';
+                deleteBtn.textContent = 'Delete';
+                td3.appendChild(editBtn);
+                td3.appendChild(deleteBtn);
 
-            let urlWithId = url + `/${r[0]}`;
-            deleteBtn.addEventListener('click',()=>{
-                deleteBook(urlWithId);
-                tr.remove();
-            })
-            editBtn.addEventListener('click',()=>{    
-                editBook(urlWithId,r[1].title,r[1].author);
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                tr.appendChild(td3);
+
+                document.querySelector('tbody').appendChild(tr);
+
+                let urlWithId = url + `/${r[0]}`;
+                deleteBtn.addEventListener('click', () => {
+                    deleteBook(urlWithId);
+                    tr.remove();
+                })
+                editBtn.addEventListener('click', () => {
+                    let title = document.getElementById('title');
+                    let author = document.getElementById('author');
+                    let form = document.querySelector('form h3');
+                    let save = document.createElement('button');
+                       editBook(urlWithId,title.value,author.value);
+
+                    form.textContent = 'Edit FORM';
+                    title.value = r[1].title;
+                    author.value = r[1].author;
+                    save.textContent = 'Save';
+
+                    let sub = document.querySelector('form button');
+                    sub.style.display = 'none';
+                    document.querySelector('form').appendChild(save);
+
+                    save.addEventListener('click',(e)=>{
+                        e.preventDefault();
+                        editBook(urlWithId,title.value,author.value);
+                        loadBooks(url);
+                        sub.style.display = 'block';
+                        save.style.display = 'none';
+                        form.textContent = 'FORM';
+                        title.value = '';
+                        author.value = '';
+                    })
+                })
             })
         })
-    })
 }
-function deleteBook(url){
-    fetch(url,{
+function deleteBook(url) {
+    fetch(url, {
         method: 'delete'
     })
 
 }
-function editBook(url,title,author){
+function editBook(url, title, author) {
 
     let record = {
         author: author,
         title: title,
     }
 
-    fetch(url,{
+    fetch(url, {
         method: 'put',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(record)
     })
 }
