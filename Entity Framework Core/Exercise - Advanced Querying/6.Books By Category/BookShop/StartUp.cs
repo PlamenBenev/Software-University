346809e7288860ba29f83576345ab28d966abc2c
Plamen Benev <benevi95@gmail.com>
2023-03-13 20:57:19 +0200

    Exercise task added

---
+++ Entity Framework Core/Exercise - Advanced Querying/6.Books By Category/BookShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByCategory(db, "horror mystery drama"));
+        }
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            var categories = input.ToLower().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+            var booksByCategory = context.Books
+                .Where(b => b.BookCategories
+                    .Any(bc => categories.Contains(bc.Category.Name.ToLower())))
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+            return string.Join(Environment.NewLine, booksByCategory);
+        }
+    }
+}
+
+
