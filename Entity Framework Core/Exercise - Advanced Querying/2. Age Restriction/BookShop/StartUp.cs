9b9b3711eb586f29f0459e899be27a316fb13eb7
Plamen Benev <benevi95@gmail.com>
2023-03-13 17:04:25 +0200

    Exercise task added

---
+++ Entity Framework Core/Exercise - Advanced Querying/2. Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, "miNor"));
+        }
+
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var bookTitles = context.Books
+                .Where(x => Enum.Parse<AgeRestriction>(command, true) == x.AgeRestriction)
+                .Select(x => x.Title)
+                .OrderBy(x => x)
+                .ToArray();
+
+            foreach (var b in bookTitles)
+            {
+                sb.AppendLine($"{b}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
---
+++ Entity Framework Core/Exercise - Advanced Querying/3. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetGoldenBooks(db));
+        }
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var goldenBooks = context.Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .Select(b => new { b.Title , b.BookId})
+                .OrderBy(b => b.BookId)
+                .ToArray();
+
+            foreach (var book in goldenBooks) { sb.AppendLine(book.Title); }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
+
+
