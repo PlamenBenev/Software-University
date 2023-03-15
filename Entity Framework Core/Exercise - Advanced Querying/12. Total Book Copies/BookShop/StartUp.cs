5449961d204a68d41237b4ce589039164f17da63
Plamen Benev <benevi95@gmail.com>
2023-03-15 21:52:05 +0200

    Exercise task added

---
+++ Entity Framework Core/Exercise - Advanced Querying/12. Total Book Copies/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(CountCopiesByAuthor(db));
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            var authorCopies = context.Authors
+                .OrderByDescending(x => x.Books.Sum(b => b.Copies))
+                .Select(x => $"{x.FirstName} {x.LastName} - {x.Books.Sum(b => b.Copies)}");
+
+            return string.Join(Environment.NewLine, authorCopies);
+        }
+    }
+}
+
+
