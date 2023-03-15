657b79342b3af5362017ecdbf9d7d6c435cc24eb
Plamen Benev <benevi95@gmail.com>
2023-03-15 20:19:46 +0200

    Exercise task added

---
+++ Entity Framework Core/Exercise - Advanced Querying/9. Book Search/BookShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBookTitlesContaining(db, "sK"));
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            var bookTitles = context.Books
+                .Where(x => x.Title.ToLower().Contains(input.ToLower()))
+                .OrderBy(x => x.Title)
+                .Select(x => x.Title);
+
+            return string.Join(Environment.NewLine, bookTitles);
+        }
+    }
+}
+
+
