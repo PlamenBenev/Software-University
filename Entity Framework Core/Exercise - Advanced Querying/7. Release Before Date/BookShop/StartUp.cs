3d5f25761af4a4115879ac2b2ab2debd597abee9
Plamen Benev <benevi95@gmail.com>
2023-03-15 19:58:40 +0200

    Exercise task added

---
+++ Entity Framework Core/Exercise - Advanced Querying/7. Release Before Date/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksReleasedBefore(db, "12-04-1992"));
+        }
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var booksReleasedBefore = context.Books
+                .Where(b => b.ReleaseDate < DateTime
+                    .ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture))
+                .OrderByDescending(b => b.ReleaseDate)
+                .Select(b => $"{b.Title} - {b.EditionType} - ${b.Price}");
+
+            return string.Join(Environment.NewLine, booksReleasedBefore);
+        }
+    }
+}
+
+
